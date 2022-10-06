5caf16c2fafe5b6d5fced705c146d74b60d43529
yuuki-maeno <yuki.maeno@lac.co.jp>
2022-10-06 11:17:26 +0000

    Added the second tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "yuuki-maeno-2"
     workspaces {
       name = "hashicat-aws"
