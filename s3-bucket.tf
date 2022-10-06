2b01881aeb7c3084b68030a11e6db97e769f33cd
yuuki-maeno <yuki.maeno@lac.co.jp>
2022-10-06 11:46:39 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/YOURNAME-training/s3-bucket/aws"
+  source  = "app.terraform.io/yuuki-maeno-2/s3-bucket/aws"
   version = "1.0.0"
   bucket_prefix = "maeno"
 }
